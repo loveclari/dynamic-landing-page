6743c24359587e725b4ee78949af8e9edaed8cdb
loveclari <clarissa.celestino@gmail.com>
2021-03-18 23:06:39 -0700

    changing classes on scroll

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,34 +1,28 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
+// /**
+//  *
+//  * Manipulating the DOM exercise.
+//  * Exercise programmatically builds navigation,
+//  * scrolls to anchors from navigation,
+//  * and highlights section in viewport upon scrolling.
+//  *
+//  * Dependencies: None
+//  *
+//  * JS Version: ES2015/ES6
+//  *
+//  * JS Standard: ESlint
+//  *
+//  */
 
-/**
- * Define Global Variables
- *
- */
-//first variable gets element id
-//second variable selects all sections
-//variable selection parent id and query selector for all sections
+// /**
+//  * Define Global Variables
+//  *
+//  */
+// //first variable gets element id
+// //second variable selects all sections
+// //variable selection parent id and query selector for all sections
 const navbar = document.getElementById('navbar__list');
 const section = document.querySelectorAll('section');
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-//Functional code with variable with an empty array
 const LandingPageNav = () => {
     let navList = [];
 
@@ -43,28 +37,26 @@ const LandingPageNav = () => {
     navbar.innerHTML = navList;
 };
 
+//calling my function
 LandingPageNav();
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
+//determining the position on viewport and returning the position
 
-// build the nav
+const activeSection = (section) => {
+    let position = section.getBoundingClientRect().top;
+    return position;
+};
 
-// Add class 'active' to section when near top of viewport
+//creating a function with a condition and a loop for each section if > or < than to add or remove a class
 
-// Scroll to anchor ID using scrollTO event
+const makeActive = () => {
+    section.forEach((section) => {
+        const changeClass = activeSection(section);
+        if (changeClass < 100 && changeClass > -100) {
+            section.classList.add('your-active-class');
+        } else section.classList.remove('your-active-class');
+    });
+};
 
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+//calling the eventlistener
+this.addEventListener('scroll', makeActive);
